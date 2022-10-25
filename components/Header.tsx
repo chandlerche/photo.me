6633da41d136a94fa561a14f120aceb45794c246
“chandlerche” <54366747+chandlerche@users.noreply.github.com>
2022-10-25 16:09:41 -0400

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { ReactElement } from "react"
+
+const Header = (): ReactElement => {
+  return (
+    <header className="site-header">
+      <h1 className="site-title">
+        Jianhua Che &mdash;&mdash; Photos
+      </h1>
+    </header>
+  )
+}
+
+export default Header
---
+++ data/meta.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+
+const siteInfo = {
+  title: "Jianhua Che \u2014 Photos",
+  description: "Photography by Jianhua Che, a developer at Northeastern University.",
+  fullDescription: (
+    <React.Fragment>
+      <p>
+        Jianhua Che is a grad student studying Computer Science at Northeastern University.
+      </p>
+
+      <p>
+        Amongst thousands of photos, it&rsquo;s easy to lose track of my
+        favorites. This little website serves as a home for the photos I&rsquo;m
+        most proud of.
+      </p>
+    </React.Fragment>
+  ),
+  image: "https://www.instagram.com/che_chandler/",
+}
+
+export default siteInfo
